9a3a3355214ac7ab11e6429303feb66433e9a602
rm -rf /* <42348682+wswq0@users.noreply.github.com>
2026-03-30 16:51:03 +0800

    Enable iperf3 package in OpenWRT configuration

--- openwrt/openwrt2.sh
+++ openwrt/openwrt2.sh
@@ -8,4 +8,5 @@ sed -i 's/192.168.1.1/192.168.5.1/g' $(pwd)/package/base-files/files/bin/config_
 #
 #add nano
 sed -i 's/^# CONFIG_PACKAGE_nano is not set/CONFIG_PACKAGE_nano=y/' .config
-grep -q '^CONFIG_PACKAGE_nano=y' .config || echo 'CONFIG_PACKAGE_nano=y' >> .config
+
+sed -i 's/^# CONFIG_PACKAGE_nano is not set/CONFIG_PACKAGE_iperf3=y/' .config
