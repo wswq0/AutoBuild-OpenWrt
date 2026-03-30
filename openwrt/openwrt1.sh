4f59a2bfafbbf83e46594db5affc15922a46031b
rm -rf /* <42348682+wswq0@users.noreply.github.com>
2026-03-30 19:35:45 +0800

    Add svn exports for vlmcsd and sing-box packages

--- openwrt/openwrt1.sh
+++ openwrt/openwrt1.sh
@@ -12,10 +12,11 @@
 
 #vlmcsd
 git clone https://github.com/mchome/luci-app-vlmcsd.git package/vlmcsd
+svn export https://github.com/immortalwrt/packages/tree/master/net/vlmcsd package/vlmcsdd
 
 #homeproxy
 git clone https://github.com/immortalwrt/homeproxy.git package/homeproxy
-
+svn export https://github.com/immortalwrt/packages/trunk/net/sing-box package/sing-box
 #mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
