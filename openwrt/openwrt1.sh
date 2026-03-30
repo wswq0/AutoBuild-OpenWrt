844631423f1e4936150a070d95d5c6e6c8c7d732
rm -rf /* <42348682+wswq0@users.noreply.github.com>
2026-03-30 16:48:24 +0800

    Add multiple git clone commands for packages

--- openwrt/openwrt1.sh
+++ openwrt/openwrt1.sh
@@ -8,3 +8,14 @@
 
 #OpenAppFilter   
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter    
+
+
+#vlmcsd
+git clone https://github.com/mchome/luci-app-vlmcsd.git package/vlmcsd
+
+#homeproxy
+git clone https://github.com/immortalwrt/homeproxy.git package/homeproxy
+
+#mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
