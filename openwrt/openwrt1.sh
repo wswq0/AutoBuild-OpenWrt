deaee66a0a66e3286bbfac4f5374ac8d13859a31
rm -rf /* <42348682+wswq0@users.noreply.github.com>
2026-03-30 19:47:47 +0800

    Update feed sources and clone repositories

--- openwrt/openwrt1.sh
+++ openwrt/openwrt1.sh
@@ -5,18 +5,19 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 #OpenAppFilter   
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter    
 
 
 #vlmcsd
-git clone https://github.com/mchome/luci-app-vlmcsd.git package/vlmcsd
-svn export https://github.com/immortalwrt/packages/tree/master/net/vlmcsd package/vlmcsdd
+git clone https://github.com/openwrt-develop/openwrt-vlmcsd.git package/vlmcsd
+git clone https://github.com/mchome/luci-app-vlmcsd.git package/luci-app-vlmcsd
 
 #homeproxy
 git clone https://github.com/immortalwrt/homeproxy.git package/homeproxy
-svn export https://github.com/immortalwrt/packages/trunk/net/sing-box package/sing-box
 #mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
