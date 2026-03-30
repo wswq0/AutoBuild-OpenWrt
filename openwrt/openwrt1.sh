7d0ce985d2a182be1d562dda922eedf649f1e4c3
rm -rf /* <42348682+wswq0@users.noreply.github.com>
2026-03-30 22:46:46 +0800

    Comment out git clone commands and update feeds
    Comment out several git clone commands and update feed sources.

--- openwrt/openwrt1.sh
+++ openwrt/openwrt1.sh
@@ -5,19 +5,22 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
+sed -i 's#https://git.openwrt.org/project/luci.git#https://github.com/immortalwrt/luci.git#g' feeds.conf.default
+sed -i 's#https://git.openwrt.org/feed/packages.git#https://github.com/immortalwrt/packages.git#g' feeds.conf.default
 
 #OpenAppFilter   
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter    
 
 
 #vlmcsd
-git clone https://github.com/openwrt-develop/openwrt-vlmcsd.git package/vlmcsd
-git clone https://github.com/mchome/luci-app-vlmcsd.git package/luci-app-vlmcsd
+#git clone https://github.com/openwrt-develop/openwrt-vlmcsd.git package/vlmcsd
+#git clone https://github.com/mchome/luci-app-vlmcsd.git package/luci-app-vlmcsd
 
 #homeproxy
-git clone https://github.com/immortalwrt/homeproxy.git package/homeproxy
+#git clone https://github.com/immortalwrt/homeproxy.git package/homeproxy
 #mosdns
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
