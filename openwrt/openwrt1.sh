865bc2a16f11b0b0f9e93c311fb167f8f599613b
rm -rf /* <42348682+wswq0@users.noreply.github.com>
2026-03-31 13:57:54 +0800

    Enable cloning of mosdns and v2ray-geodata
    Uncommented lines to clone mosdns and v2ray-geodata repositories.

--- openwrt/openwrt1.sh
+++ openwrt/openwrt1.sh
@@ -7,8 +7,8 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
-sed -i 's#https://git.openwrt.org/project/luci.git#https://github.com/immortalwrt/luci.git#g' feeds.conf.default
-sed -i 's#https://git.openwrt.org/feed/packages.git#https://github.com/immortalwrt/packages.git#g' feeds.conf.default
+#sed -i 's#https://git.openwrt.org/project/luci.git#https://github.com/immortalwrt/luci.git#g' feeds.conf.default
+#sed -i 's#https://git.openwrt.org/feed/packages.git#https://github.com/immortalwrt/packages.git#g' feeds.conf.default
 
 #OpenAppFilter   
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter    
@@ -21,6 +21,6 @@ sed -i 's#https://git.openwrt.org/feed/packages.git#https://github.com/immortalw
 #homeproxy
 #git clone https://github.com/immortalwrt/homeproxy.git package/homeproxy
 #mosdns
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
