0a9625e1e07f3e2fa4590398369711a421a83b8d
rm -rf /* <42348682+wswq0@users.noreply.github.com>
2026-04-02 14:53:46 +0800

    Add luci-app-adguardhome installation steps
    Clone luci repository and copy luci-app-adguardhome.

--- immortalwrt/immortalwrt1.sh
+++ immortalwrt/immortalwrt1.sh
@@ -7,3 +7,13 @@
 
 #OpenAppFilter    
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter    
+
+#add luci-app-adguardhome
+git clone --depth=1 \
+  https://github.com/coolsnowwolf/luci.git \
+  -b openwrt-25.12 \
+  /tmp/luci
+
+mkdir -p luci
+cp -r /tmp/luci/applications/luci-app-adguardhome luci/
+rm -rf /tmp/luci
